dd9d2d5534547248e866404335bfab3178382c52
MivianCoin <64926297+MivianTKN@users.noreply.github.com>
2021-10-25 18:13:54 +0000

    Initial commit

---
+++ bp/scripts/Main.js
@@ -0,0 +1,24 @@
+import { World, Commands, TickEventSignal } from "mojang-minecraft"
+//Start callback
+TickEventSignal.subscribe(tick => {
+  onTick()
+})
+
+//Functionu to check game time
+function onTick(){
+  let gameTime = Commands.run("time query daytime", World.getDimension("overworld"))
+  let newRes = gameTime.result
+  //converts result to int
+  let gameTime = parseInt(newRes.data)
+  //converts result to string and says in chat for debugging
+  Commands.run("say " + toString(gameTime), World.getDimension("overworld"))
+  
+}
+// Starts a Blood MOon - Summons Zombies
+function startMoon(){
+  let players = world.getPlayers()
+  for i in players(){
+    let coords = players.i.getCoordinates()
+    commands.Run("summon zombie" + coords, World.getDimensions("overworld"))
+  }
+}
